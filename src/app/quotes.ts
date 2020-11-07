cdc4000a66b899a55df07c0cb7c7af8830c8a6b7
Preston-Too <toopreston@gmail.com>
2020-11-07 14:35:44 +0300

    add constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,9 @@
 export class Quotes {
+    public upvote: number;
+    public downvote: number;
+    constructor(public name: string, public author: string, public quoteDescription: string, public date: Date) {
+        
+
+    }
 }
+
