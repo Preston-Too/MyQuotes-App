12c13bea426b5f2f72f9369f5862f785a532bafe
Preston-Too <toopreston@gmail.com>
2020-11-07 15:34:57 +0300

    add an array of quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from 'src/app/quotes';
 
 @Component({
   selector: 'app-quote',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes = [
+    new Quotes('Preston', 'Mutahi Kagwe', 'If we behave normally,Covid-19 disease will treat us upnormally.', new Date('2020/10/4')),
+    new Quotes('Alex', 'Barack Obama', 'If you’re walking down the right path and you’re willing to keep walking, eventually you’ll make progress', new Date('2020/9/2')),
+    new Quotes('Luciah', 'Mahatma Gandhi', 'Live as if you were to die tomorrow. Learn as if you were to live forever.', new Date('2020/10/13')),
+    new Quotes('Ronald', 'Lionel Messi', 'You have to fight to reach your dream. You have to sacrifice and work hard for it', new Date('2020/8/26'))
+    
+  ]
+  
+ 
   constructor() { }
 
   ngOnInit(): void {
