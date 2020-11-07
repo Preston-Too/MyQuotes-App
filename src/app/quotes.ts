adfcd1b4731afeaef080dc5d8bf29d047113f7c9
Preston-Too <toopreston@gmail.com>
2020-11-07 14:37:11 +0300

    add upvote and downvote

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,7 +2,8 @@ export class Quotes {
     public upvote: number;
     public downvote: number;
     constructor(public name: string, public author: string, public quoteDescription: string, public date: Date) {
-        
+        this.upvote = 0;
+        this.downvote = 0;
 
     }
 }
