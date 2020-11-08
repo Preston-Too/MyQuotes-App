394bcfe05fe367a31002fdee6209f95e6f84fc19
Preston-Too <toopreston@gmail.com>
2020-11-08 12:37:05 +0300

    add function to display new quote

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesFormComponent implements OnInit {
 
+  name: string;
+  author: string;
+  quote: string;
+  quoteDescription: string;
+  newQuote: any;
+  date: Date;
+  @Output() emitNewQuote = new EventEmitter()
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.date = new Date(this.date);
+    this.newQuote = new Quotes(this.name, this.author, this.quoteDescription, this.date)
+    this.name = '';
+    this.author = '';
+    this.quoteDescription = '';
+    this.emitNewQuote.emit(this.newQuote)
+
+  }
 }
