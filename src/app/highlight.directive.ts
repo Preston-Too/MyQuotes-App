ec5f872994e5798a4faa89fffd1f12787b70908f
Preston-Too <toopreston@gmail.com>
2020-11-08 13:10:23 +0300

    add background and color styles

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(element: ElementRef) { 
+    element.nativeElement.style.backgroundColor = 'blue';
+     element.nativeElement.style.color = 'white'
+  }
 
 }
